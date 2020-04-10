62a771caa57c12f85cacec03aa96dc716c0204b8
Matias Gutierrez <gutierrez.matias@outlook.com>
2020-04-10 00:51:00 -0300

    define development run of the app

--- wsgi.py
+++ wsgi.py
@@ -5,6 +5,8 @@ from werkzeug import run_simple
 
 if __name__ == '__main__':
     app = create_app()
-    ctx = ssl.SSLContext(ssl.PROTOCOL_SSLv23)
-    ctx.load_cert_chain('localhost.crt', 'localhost.key')
-    run_simple('0.0.0.0', 5000, app, ssl_context=ctx)
+    is_development = app.config.get('DEVELOPMENT')
+    if is_development:
+        ctx = ssl.SSLContext(ssl.PROTOCOL_SSLv23)
+        ctx.load_cert_chain('localhost.crt', 'localhost.key')
+        run_simple('0.0.0.0', 5000, app, ssl_context=ctx)
