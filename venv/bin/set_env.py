e6e5d66a83b4904de42d6914b2a45e41054bc5a6
Matias Gutierrez <gutierrez.matias@outlook.com>
2020-04-10 00:31:29 -0300

    remove files that should've been ignored

--- venv/bin/set_env.py
+++
@@ -1,5 +0,0 @@
-import os
-
-os.environ['GOOGLE_CLIENT_ID'] = '452758247766-sisbc4uart9knl6jeihdn8rbgrjoadb5.apps.googleusercontent.com'
-os.environ['GOOGLE_CLIENT_SECRET'] = 'KV-V0fvNHh1-ncG9CuYFFSWz'
-os.environ['GOOGLE_CLIENT_ID'] = 'https://accounts.google.com/.well-known/openid-configuration'
