f2bfc5e0c8985074a09f7b7cf654692a37f1c172
Matias Gutierrez <gutierrez.matias@outlook.com>
2020-04-11 19:02:53 -0300

    heroku deployment

--- wsgi.py
+++ wsgi.py
@@ -10,3 +10,5 @@ if __name__ == '__main__':
         ctx = ssl.SSLContext(ssl.PROTOCOL_SSLv23)
         ctx.load_cert_chain('localhost.crt', 'localhost.key')
         run_simple('0.0.0.0', 5000, app, ssl_context=ctx)
+    else:
+        app.run()
