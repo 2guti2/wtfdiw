91092665513e23e9d66a2078ed03cd3fdfe1bcf1
Matias Gutierrez <gutierrez.matias@outlook.com>
2020-04-11 19:23:32 -0300

    prod and dev startup files

--- wsgi.py
+++ wsgi.py
@@ -1,14 +1,11 @@
 from application import create_app
 import ssl
-import os
 from werkzeug import run_simple
 
-app = create_app()
-is_development = app.config.get('DEVELOPMENT')
-if is_development:
-    ctx = ssl.SSLContext(ssl.PROTOCOL_SSLv23)
-    ctx.load_cert_chain('localhost.crt', 'localhost.key')
-    run_simple('0.0.0.0', 5000, app, ssl_context=ctx)
-else:
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+if __name__ == '__main__':
+    app = create_app()
+    is_development = app.config.get('DEVELOPMENT')
+    if is_development:
+        ctx = ssl.SSLContext(ssl.PROTOCOL_SSLv23)
+        ctx.load_cert_chain('localhost.crt', 'localhost.key')
+        run_simple('0.0.0.0', 5000, app, ssl_context=ctx)
