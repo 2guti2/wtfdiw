b424b88c8458de481d9654f5cc84d455d8535299
Matias Gutierrez <gutierrez.matias@outlook.com>
2020-04-11 19:14:06 -0300

    wsgi

--- wsgi.py
+++ wsgi.py
@@ -2,15 +2,11 @@ from application import create_app
 import ssl
 from werkzeug import run_simple
 
-global heroku_app
-
-if __name__ == '__main__':
-    app = create_app()
-    is_development = app.config.get('DEVELOPMENT')
-    if is_development:
-        ctx = ssl.SSLContext(ssl.PROTOCOL_SSLv23)
-        ctx.load_cert_chain('localhost.crt', 'localhost.key')
-        run_simple('0.0.0.0', 5000, app, ssl_context=ctx)
-    else:
-        heroku_app = app
-        heroku_app.run()
+app = create_app()
+is_development = app.config.get('DEVELOPMENT')
+if is_development:
+    ctx = ssl.SSLContext(ssl.PROTOCOL_SSLv23)
+    ctx.load_cert_chain('localhost.crt', 'localhost.key')
+    run_simple('0.0.0.0', 5000, app, ssl_context=ctx)
+else:
+    app.run()
