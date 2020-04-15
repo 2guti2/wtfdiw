caad5996df117dce4e9410d7c49e377fdf480437
Matias Gutierrez <gutierrez.matias@outlook.com>
2020-04-14 23:15:58 -0300

    refactor

--- application/app_module.py
+++ application/app_module.py
@@ -1,16 +1,7 @@
-import json
-import requests
 from flask_sqlalchemy import SQLAlchemy
 from injector import Module, singleton
 from application.factories.database import db, migrate
-from application.factories.socketio import socket_io
-from application.factories.oauth_client import oauth
-from application.user.models.session_model import Session
-from application.user.models.user_model import User
-from application.user.services.google_service import GoogleService
-from application.user.services.session_service import SessionService
-from application.user.services.user_service import UserService
-from flask_socketio import SocketIO
+from application.user.user_module import configure_user_module
 
 
 class AppModule(Module):
@@ -19,17 +10,8 @@ class AppModule(Module):
 
     def configure(self, binder):
         db_instance = self.configure_db(self.app)
-        google_service_instance = GoogleService(self.app, json, oauth, requests)
-        user_service_instance = UserService(db_instance, User)
-        session_service_instance = SessionService(
-            db_instance, Session, User, google_service_instance, user_service_instance
-        )
-
         binder.bind(SQLAlchemy, to=db_instance, scope=singleton)
-        binder.bind(GoogleService, to=google_service_instance, scope=singleton)
-        binder.bind(UserService, to=user_service_instance, scope=singleton)
-        binder.bind(SessionService, to=session_service_instance, scope=singleton)
-        binder.bind(SocketIO, to=socket_io, scope=singleton)
+        configure_user_module(self.app, db_instance, binder)
 
     # noinspection PyMethodMayBeStatic
     def configure_db(self, app):
---
+++ application/user/user_module.py
@@ -0,0 +1,24 @@
+import json
+import requests
+from application.factories.socketio import socket_io
+from application.factories.oauth_client import oauth
+from application.user.models.session_model import Session
+from application.user.models.user_model import User
+from application.user.services.google_service import GoogleService
+from application.user.services.session_service import SessionService
+from application.user.services.user_service import UserService
+from flask_socketio import SocketIO
+from injector import singleton
+
+
+def configure_user_module(app, db, binder):
+    google_service_instance = GoogleService(app, json, oauth, requests)
+    user_service_instance = UserService(db, User)
+    session_service_instance = SessionService(
+        db, Session, User, google_service_instance, user_service_instance
+    )
+
+    binder.bind(GoogleService, to=google_service_instance, scope=singleton)
+    binder.bind(UserService, to=user_service_instance, scope=singleton)
+    binder.bind(SessionService, to=session_service_instance, scope=singleton)
+    binder.bind(SocketIO, to=socket_io, scope=singleton)
